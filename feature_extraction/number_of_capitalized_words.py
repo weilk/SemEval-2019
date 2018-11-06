796373ce1530a95d11023325834308cae4a07887
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2018-11-07 00:44:24 +0200

    adding an example of failure

--- feature_extraction/number_of_capitalized_words.py
+++ feature_extraction/number_of_capitalized_words.py
@@ -5,4 +5,4 @@ class number_of_capitalized_words(feature_extraction):
     
     def run(self,D,columns = []):
         for column in columns:
-            D['number_of_capitalized_words_{}'.format(column)] = D[column].apply(lambda x:sum([i.strip(string.punctuation).isalpha() for i in x.split() if i.strip(string.punctuation).isalpha() and i.strip(string.punctuation).upper() == i.strip(string.punctuation) and len(i.strip(string.punctuation)) > 0]))
+            D['number_of_capitalized_words_{}'.format(column)] = D[column].apply(lambda x:sum([i.strip(string.punctuation).isalpha() for i in x.split() if i.strip(string.punctuation).isalpha() and i.strip(string.punctuation).upper() == i.strip(string.punctuation) and len(i.strip(string.punctuation)) == 0]))
