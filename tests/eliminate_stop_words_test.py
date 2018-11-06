c780aed5d2b154acfe6975a780f2fd0537bd9d42
ingridstoleru <istoleru@ias.bitdefender.biz>
2018-11-07 01:01:39 +0200

    Adapted test for dataframe

--- tests/eliminate_stop_words_test.py
+++ tests/eliminate_stop_words_test.py
@@ -7,9 +7,11 @@ from preprocess import eliminate_stop_words
 class TestEliminateStopWords(unittest.TestCase):
 
     def test_run(self):
-        D = pd.DataFrame({'x':["","their result was good and bad"],"y":["","result good bad"]})
+        D = pd.DataFrame({'x':["","their result was good and bad"]})
+        expected = pd.DataFrame({"x":["","result good bad"]})
+
         eliminate_stop_words.run(D,["x"])
-        pd.testing.assert_series_equal(D["x"],D["y"])
+        pd.testing.assert_frame_equal(D,expected)
 
 if __name__ == '__main__':
     unittest.main()
