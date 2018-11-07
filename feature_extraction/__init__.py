86be1591aa7f992ad53a6e5784155d1163dfbf53
ingridstoleru <istoleru@ias.bitdefender.biz>
2018-11-07 10:19:47 +0200

    Added implementations and tests

--- feature_extraction/__init__.py
+++ feature_extraction/__init__.py
@@ -3,9 +3,11 @@ from feature_extraction.number_of_capitalized_words import *
 from feature_extraction.number_negation_words import *
 from feature_extraction.number_of_elongated_words import *
 from feature_extraction.number_boosting_words import *
+from feature_extraction.number_question_marks import *
 
 number_of_words = number_of_words(0,"number_of_words")
 number_of_capitalized_words = number_of_capitalized_words(0, "number_of_capitalized_words")
 number_of_elongated_words = number_of_elongated_words(0, "number_of_elongated_words")
 number_negation_words = number_negation_words(0, "number_negation_words")
 number_boosting_words = number_boosting_words(0, "number_boosting_words")
+number_question_marks = number_question_marks(0, "number_question_marks")
---
+++ feature_extraction/number_question_marks.py
@@ -0,0 +1,16 @@
+from classes import preprocess
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
+class number_question_marks(preprocess):
+
+    def run(self,D,columns):
+        for column in columns:
+            tokenized_words = D[column].apply(word_tokenize)
+            D['number_of_question_marks_{}'.format(column)] = tokenized_words.apply(lambda x: len([w for w in x if w == "?"]))
+        
+
