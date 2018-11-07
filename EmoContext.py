39f99a8b2f289a685a99e3882ddad5bb0f444e46
Cristian Simionescu <Cristian Simionescu>
2018-11-07 09:55:58 +0200

    BIG BOIO

--- EmoContext.py
+++ EmoContext.py
@@ -12,19 +12,15 @@ emocontext_DataFrame = functions.parse_file(r"raw_data/EmoContext/train.txt", "E
 
 features = []
 pp=[(make_lower_case,["turn1","turn2","turn3"])]
-fe=[(number_of_words,["turn1","turn2","turn3"])]
+fe=[(number_of_words,["turn1","turn2","turn3"]),(number_of_capitalized_words,["turn1","turn2","turn3"]),(number_of_elongated_words,["turn1","turn2","turn3"]),(number_negation_words,["turn1","turn2","turn3"])]
 
 data_object = data(raw=emocontext_DataFrame,pp=pp,fe=fe)
+data_object.D = data_object.D.drop(input_emocontext,axis=1)
 
-print(data_object.D)
+msk = np.random.rand(len(data_object.D)) < 0.8
 
-"""
-exit()
-print(best_fit.run(data_object.D))
-simple_MLP.train(data_object.D)
-simple_MLP.test(data_object.D)
-print(simple_MLP.forward_pass(data_object.D))
-"""
+simple_MLP.train(data_object.D[msk])
+simple_MLP.test(data_object.D[~msk])
 
 # docker build -t simi2525/ml-env:cpu -f Dockerfile.cpu .
 # docker run -it -p 8888:8888 -p 6006:6006  -v ${PWD}/jupyter_notebook_config.py:/root/.jupyter/jupyter_notebook_config.py -v ${PWD}:"/root/SemEval-2019" simi2525/ml-env:cpu
--- model/simple_MLP.py
+++ model/simple_MLP.py
@@ -1,4 +1,10 @@
 from classes import model
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.preprocessing.text import one_hot
+from utils import *
 
 class simple_MLP(model):
     
@@ -7,8 +13,21 @@ class simple_MLP(model):
     
 
     def train(self,D):
+        self.data = D.drop(output_emocontext,axis=1).values
+        self.labels = pd.get_dummies(D[output_emocontext]).values
+
+        self.model = Sequential()
+        self.model.add(Dense(32, activation='relu', input_dim=self.data.shape[1]))
+        self.model.add(Dense(4, activation='softmax'))
+        
+        self.model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
+
+        self.model.fit(self.data, self.labels, epochs=5, batch_size=32)
         print("Done training")
 
 
     def test(self,D):
+        self.data = D.drop(output_emocontext,axis=1).values
+        self.labels = pd.get_dummies(D[output_emocontext])
+        print(self.model.evaluate(self.data, self.labels, batch_size=128))
         print("Done testing")
