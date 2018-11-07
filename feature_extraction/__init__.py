e3ea706da6b02ae32177178db7fbecadbf44dd2c
ingridstoleru <istoleru@ias.bitdefender.biz>
2018-11-07 10:13:08 +0200

    Added implementations and tests

--- feature_extraction/__init__.py
+++ feature_extraction/__init__.py
@@ -2,8 +2,10 @@ from feature_extraction.number_of_words import *
 from feature_extraction.number_of_capitalized_words import *
 from feature_extraction.number_negation_words import *
 from feature_extraction.number_of_elongated_words import *
+from feature_extraction.number_boosting_words import *
 
 number_of_words = number_of_words(0,"number_of_words")
 number_of_capitalized_words = number_of_capitalized_words(0, "number_of_capitalized_words")
 number_of_elongated_words = number_of_elongated_words(0, "number_of_elongated_words")
 number_negation_words = number_negation_words(0, "number_negation_words")
+number_boosting_words = number_boosting_words(0, "number_boosting_words")
