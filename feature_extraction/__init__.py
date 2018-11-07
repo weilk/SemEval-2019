8bb56561e667fde49b049adac25be19758734b77
ingridstoleru <istoleru@ias.bitdefender.biz>
2018-11-07 10:27:40 +0200

    Added implementations and tests

--- feature_extraction/__init__.py
+++ feature_extraction/__init__.py
@@ -1,13 +1,14 @@
 from feature_extraction.number_of_words import *
 from feature_extraction.number_of_capitalized_words import *
-from feature_extraction.number_negation_words import *
 from feature_extraction.number_of_elongated_words import *
+from feature_extraction.number_exclamation_marks import *
 from feature_extraction.number_boosting_words import *
 from feature_extraction.number_question_marks import *
+from feature_extraction.number_negation_words import *
 
 number_of_words = number_of_words(0,"number_of_words")
 number_of_capitalized_words = number_of_capitalized_words(0, "number_of_capitalized_words")
 number_of_elongated_words = number_of_elongated_words(0, "number_of_elongated_words")
 number_negation_words = number_negation_words(0, "number_negation_words")
 number_boosting_words = number_boosting_words(0, "number_boosting_words")
-number_question_marks = number_question_marks(0, "number_question_marks")
+number_exclamation_marks = number_exclamation_marks(0, "number_exclamation_marks")
