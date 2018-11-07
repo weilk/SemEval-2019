55272d53d7e357933ae9b2ed8aacabbbc2abc171
ingridstoleru <istoleru@ias.bitdefender.biz>
2018-11-07 02:52:41 +0200

    Added new test

--- feature_extraction/__init__.py
+++ feature_extraction/__init__.py
@@ -1,4 +1,7 @@
 from feature_extraction.number_of_words import *
 from feature_extraction.number_of_capitalized_words import *
+from feature_extraction.number_negation_words import *
+
 number_of_words = number_of_words(0,"number_of_words")
 number_of_capitalized_words = number_of_capitalized_words(0, "number_of_capitalized_words")
+number_negation_words = number_negation_words(0, "tokenization")
