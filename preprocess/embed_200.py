bf40b692fb0001d94683c05cd1d75543f8a7e730
Lucaci Diana <diana.lucaci22@yahoo.com>
2018-11-21 00:26:24 +0200

    fix embed add column

--- preprocess/embed_200.py
+++ preprocess/embed_200.py
@@ -1,6 +1,7 @@
 from classes import preprocess
 from nltk.tokenize import TweetTokenizer
 import numpy as np
+import pandas as pd
 
 class embed_200(preprocess):
 
@@ -20,10 +21,14 @@ class embed_200(preprocess):
     def run(self,D,columns):
         tokenizer = TweetTokenizer()
         for column in columns:
+            embeddings = []
             for index in range(len(D[column])):
                 words = tokenizer.tokenize(D[column][index])
                 embedding = np.array(0)
                 for word in words:
                     embedding = np.append(embedding, self.embeddings.get(word, np.zeros(200)))
-                D['embedding_200_{}'.format(column)][index] = embedding
+                embeddings.append(embedding)
+            D['embedding_200_{}'.format(column)] = pd.Series(embeddings)
+        print(D.columns.values)
+        print("embedded")
         return D
