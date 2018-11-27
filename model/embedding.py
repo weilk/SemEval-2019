263ba23b6e3025fa23471bafce949043a13d0b99
Lucaci Diana <diana.lucaci22@yahoo.com>
2018-11-27 18:18:25 +0200

    model with all the features

--- model/embedding.py
+++ model/embedding.py
@@ -37,6 +37,12 @@ class embedding(model):
                             trainable=True)(input_turn)
         return input_turn, LSTM(128)(emb)
 
+    def _input_turn_embeddings(self, D):
+        emb1_input = np.array([np.array(x) for x in D['embedding_200_turn1'].values])
+        emb2_input = np.array([np.array(x) for x in D['embedding_200_turn2'].values])
+        emb3_input = np.array([np.array(x) for x in D['embedding_200_turn3'].values])
+        return [emb1_input, emb2_input, emb3_input]
+
     def train(self, D, embedding_matrix, embedding_dim=200):
         self.embedding_matrix = embedding_matrix
         self.embedding_dim = embedding_dim
@@ -44,15 +50,21 @@ class embedding(model):
         self.labels = D[output_emocontext].values
         self.data = D
         D = D.drop(output_emocontext,axis=1) # data 
-        print(D.shape)
-        print(D.columns)
-
+        emb1_input = np.array([np.array(x) for x in D['embedding_200_turn1'].values])
+        emb2_input = np.array([np.array(x) for x in D['embedding_200_turn2'].values])
+        emb3_input = np.array([np.array(x) for x in D['embedding_200_turn3'].values])
+        
+        print("Dropping emb cols: " )
+        D = D.drop(['embedding_200_turn1', 'embedding_200_turn2', 'embedding_200_turn3'], axis=1)
 
         self.model = Sequential()
 
+        features = Input(shape=(D.shape[1],), name="features_input")
         input_turn1, lstm1_out = self.add_turn_layer("1")
         input_turn2, lstm2_out = self.add_turn_layer("2")
         input_turn3, lstm3_out = self.add_turn_layer("3")
+
+        # features = Input(shape = )
 
         x = concatenate([lstm1_out, lstm2_out, lstm3_out])
         x = Dense(200, activation='relu')(x)
@@ -60,7 +72,7 @@ class embedding(model):
         x = Dense(64, activation='relu')(x)
 
         main_out = Dense(self.labels.shape[1], activation='softmax', name='output')(x)
-        self.model = Model(inputs=[input_turn1, input_turn2, input_turn3], outputs=[main_out])
+        self.model = Model(inputs=[features, input_turn1, input_turn2, input_turn3], outputs=[main_out])
         # self.model.add(SpatialDropout1D(0.2))
         # self.model.add(Bidirectional(CuDNNLSTM(128, return_sequences=True)))
         # self.model.add(Bidirectional(CuDNNLSTM(64)))
@@ -68,14 +80,8 @@ class embedding(model):
         # self.model.add(Dense(units=5, activation='softmax'))
         self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
         print(self.model.summary())
-        print(D.shape)
-        emb1_input = np.array([np.array(x) for x in D['embedding_200_turn1'].values])
-        emb2_input = np.array([np.array(x) for x in D['embedding_200_turn2'].values])
-        emb3_input = np.array([np.array(x) for x in D['embedding_200_turn3']    .values])
-        print(np.shape(emb1_input))
 
-
-        self.model.fit([emb1_input, emb2_input, emb3_input], self.labels, epochs=10, batch_size=128,validation_split=0.2)
+        self.model.fit([D, emb1_input, emb2_input, emb3_input], self.labels, epochs=10, batch_size=128,validation_split=0.2)
         print("Done training")
 
     def test(self,D):
