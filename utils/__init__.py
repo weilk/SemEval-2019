a22882155a0385dbf204ea73070e2d8be62f953d
Cristian Simionescu <Cristian Simionescu>
2019-01-15 20:34:44 +0200

    misc

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,1 @@
 from utils.functions import *
-from utils.embedding_matrix import *
-from utils.max_no_words import *
--- utils/functions.py
+++ utils/functions.py
@@ -4,8 +4,6 @@ import csv
 from keras import backend as K
 from collections import Counter
 
-encoded_output = ["label_angry","label_happy","label_others","label_sad"]
-
 list_boosting = []
 
 output_emocontext = ["label"]
@@ -13,10 +11,6 @@ output_offenseval = ["suba", "subb", "subc"]
 
 input_emocontext = ["id","turn1", "turn2", "turn3"]
 input_offenseval = ["text"]
-
-#suba - NOT (notoffensive), OFF (offensive)
-#subb - TIN (targeted insult), TTH (targeted threat), UNT (untargeted)
-#subc - IND (individual), GRP (Group), ORG (organization or entity), OTH (other)
 
 def parse_file(file_path, file_type):
 	output_dict = dict()
@@ -40,10 +34,4 @@ def parse_file(file_path, file_type):
 					output_dict[output_names[i]] = list()
 				output_dict[output_names[i]].append(row[i])
 
-
 	return pd.DataFrame(output_dict)	
-
-
-if __name__ == "__main__":
-	emocontext_DataFrame = parse_file(r"raw_data\\EmoContext\\train.txt", "EmoContext")
-	offenseval_DataFrame = parse_file(r"raw_data\\OffensEval\\offenseval-trial.txt", "OffensEval")
