601a8f17e77930a171948618b105023c71fbe0a7
Cristian Simionescu <cristian@nexusmedia.ro>
2019-01-22 15:37:20 +0200

    online jupyter notebook integration

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,11 +1,11 @@
 import os
 from IPython.lib import passwd
 
-c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.ip = '192.168.0.107'
 c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
+c.NotebookApp.open_browser = True
 # setting up the password
 from IPython.lib import passwd
 password = passwd("2525")
 c.NotebookApp.password = password
-c.MultiKernelManager.default_kernel_name = 'python36'
+c.MultiKernelManager.default_kernel_name = 'python3'
--- utils/functions.py
+++ utils/functions.py
@@ -32,28 +32,28 @@ input_emocontext = ["id","turn1", "turn2", "turn3"]
 input_offenseval = ["text"]
 
 def parse_file(file_path, file_type):
-	output_dict = dict()
-	output_names = []
-	if file_type == "EmoContext":
-		output_names = input_emocontext + output_emocontext
-	elif file_type == "OffensEval":
-		output_names = input_offenseval + output_offenseval
-	else:
-		raise NameError("Invalid file type. Options: EmoContext, OffensEval")
+    output_dict = dict()
+    output_names = []
+    if file_type == "EmoContext":
+        output_names = input_emocontext + output_emocontext
+    elif file_type == "OffensEval":
+        output_names = input_offenseval + output_offenseval
+    else:
+        raise NameError("Invalid file type. Options: EmoContext, OffensEval")
 
-	with open(file_path, newline='\n', encoding='utf8') as csvfile:
-		csvreader = csv.reader(csvfile, delimiter='\t')
-		counter = 0
-		for row in csvreader:		
-			if file_type == "EmoContext" and counter == 0:
-				counter = 1
-				continue
-			for i in range(len(row)):
-				if output_names[i] not in output_dict.keys():
-					output_dict[output_names[i]] = list()
-				output_dict[output_names[i]].append(row[i])
+    with open(file_path, newline='\n', encoding='utf8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        counter = 0
+        for row in csvreader:
+            if file_type == "EmoContext" and counter == 0:
+                counter = 1
+                continue
+            for i in range(len(row)):
+                if output_names[i] not in output_dict.keys():
+                    output_dict[output_names[i]] = list()
+                output_dict[output_names[i]].append(row[i])
 
-	return pd.DataFrame(output_dict)	
+    return pd.DataFrame(output_dict)
 
 
 import os
