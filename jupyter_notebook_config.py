f737153a1687cdd69492560b894082723c52073b
Cristian Simionescu <cristian@nexusmedia.ro>
2019-01-22 15:51:30 +0200

    Published jupyter notebooks online

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 import os
 from IPython.lib import passwd
 
-c.NotebookApp.ip = '192.168.0.107'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = True
 # setting up the password
